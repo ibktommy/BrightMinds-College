9329b3faa4038a74107c90f5792501164592779c
ibktommy <atomquartz@gmail.com>
2022-10-12 13:09:04 +0100

    Add Footer component to the APp

--- src/App.js
+++ src/App.js
@@ -5,18 +5,22 @@ import Home from "./pages/Home/Home.jsx";
 import About from "./pages/About/About.jsx";
 import Login from "./pages/Login/Login.jsx";
 import Register from "./pages/Register/Register.jsx";
+import Footer from "./components/Footer/Footer.jsx";
 
 function App() {
 	return (
-		<div className="container">
-			<Navbar />
-			<Routes>
-				<Route exact path="/" element={<Home />} />
-				<Route path="/about" element={<About />} />
-				<Route path="/login" element={<Login />} />
-				<Route path="/register" element={<Register />} />
-			</Routes>
-		</div>
+		<>
+			<div className="container">
+				<Navbar />
+				<Routes>
+					<Route exact path="/" element={<Home />} />
+					<Route path="/about" element={<About />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/register" element={<Register />} />
+				</Routes>
+			</div>
+			<Footer />
+		</>
 	);
 }
 
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { FaFacebook, FaGithub, FaTwitter } from "react-icons/fa";
+import Logo from "../Logo/Logo";
+import "./Footer.scss";
+
+const Footer = () => {
+	return (
+		<section className="footer">
+			<div className="footer-container">
+				<div className="footer-container-left">
+					<Logo />
+					<p className="para">
+						A place to learn to become <br />
+						a professional in your chosen <br />
+						course of study.
+					</p>
+					<div className="footer-container-left_socials">
+						<FaFacebook className="socials" />
+						<FaGithub className="socials" />
+						<FaTwitter className="socials" />
+					</div>
+				</div>
+				<div className="footer-container-right">
+					<div className="footer-container-right_list">
+						<h4>Courses</h4>
+						<div className="footer-container-right_list-items">
+							<p>Engineering</p>
+							<p>Accounting</p>
+							<p>Nursing</p>
+							<p>Art</p>
+						</div>
+					</div>
+					<div className="footer-container-right_list">
+						<h4>Support</h4>
+						<div className="footer-container-right_list-items">
+							<p>About Us</p>
+							<p>Careers</p>
+							<p>Blog</p>
+							<p>Blog</p>
+						</div>
+					</div>
+					<div className="footer-container-right_list">
+						<h4>Categories</h4>
+						<div className="footer-container-right_list-items">
+							<p>Scholarship</p>
+							<p>Teacher</p>
+							<p>Programmes</p>
+							<p>Tution Fee</p>
+						</div>
+					</div>
+					<div className="footer-container-right_list">
+						<h4>Contact</h4>
+						<div className="footer-container-right_list-items">
+							<p>L1 Garden Street, Ekiti, Nigeria</p>
+							<p>+234 810 123 4567</p>
+							<p>brightminds@email.com</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default Footer;
