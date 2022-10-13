3eeb4c52b8876304bd9de610ccfabc8943714fd9
ibktommy <atomquartz@gmail.com>
2022-10-13 12:38:17 +0100

    Create a saparate data-file to get items from to fill the about page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -2,11 +2,64 @@ import React from "react";
 import "./About.scss";
 import welcomeimg from "../../images/student-3.jpg";
 import { SiBookstack } from "react-icons/si";
+import { data } from "./AboutData";
 
 function About() {
 	return (
 		<main>
 			<section className="about">
+				<h3>About us</h3>
+
+				{data.map((item) => {
+					const {
+						id,
+						title,
+						header,
+						text,
+						iconOne,
+						textTwo,
+						miniTextOne,
+						iconTwo,
+						textThree,
+						miniTextTwo,
+						image,
+					} = item;
+					return (
+						<div key={id} className="about-col flex">
+							<div className="about-col_left">
+								<div className="about-col_left-img">
+									<img src={image} alt="about-img" />
+								</div>
+							</div>
+
+							<div className="about-col_right">
+								<h2>{title}</h2>
+								<h4 dangerouslySetInnerHTML={{ __html: header }} />
+								<p className="para">{text}</p>
+
+								<div className="article-content">
+									<article className="flex">
+										{iconOne}
+
+										<div>
+											<h5>{textTwo}</h5>
+											<p className="para">{miniTextOne}</p>
+										</div>
+									</article>
+									<article className="flex">
+										{iconTwo}
+										<div>
+											<h5>{textThree}</h5>
+											<p className="para">{miniTextTwo}</p>
+										</div>
+									</article>
+								</div>
+							</div>
+						</div>
+					);
+				})}
+			</section>
+			{/* <section className="about">
 				<h3>About us</h3>
 
 				<div className="about-col flex">
@@ -27,19 +80,31 @@ function About() {
 							voluptatibus, iure deleniti velit?
 						</p>
 
-						<article className="flex">
-							<SiBookstack />
-							<div>
-								<h5>Best Quality Courses</h5>
-								<p className="para">
-									Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-									Tempora possimus voluptatum pariatur maxime sapiente!
-								</p>
-							</div>
-						</article>
+						<div className="article-content">
+							<article className="flex">
+								<SiBookstack />
+								<div>
+									<h5>Best Quality Courses</h5>
+									<p className="para">
+										Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+										Tempora possimus voluptatum pariatur maxime sapiente!
+									</p>
+								</div>
+							</article>
+							<article className="flex">
+								<SiBookstack />
+								<div>
+									<h5>Best Quality Courses</h5>
+									<p className="para">
+										Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+										Tempora possimus voluptatum pariatur maxime sapiente!
+									</p>
+								</div>
+							</article>
+						</div>
 					</div>
 				</div>
-			</section>
+			</section> */}
 		</main>
 	);
 }
---
+++ src/pages/About/AboutData.js
@@ -0,0 +1,37 @@
+import { SiBookstack } from "react-icons/si";
+import { FaChalkboardTeacher } from "react-icons/fa";
+import { GiTeacher } from "react-icons/gi";
+
+export const data = [
+	{
+		id: 1,
+		title: "about us",
+		header: "welcome to brightminds <br /> admissions page",
+		text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.Exercitationem nobis odio facere dolore nesciunt esse evenietvoluptatibus, iure deleniti velit?",
+		iconOne: <SiBookstack />,
+		headerTextOne: "Best Quality Courses",
+		miniTextOne:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora possimus voluptatum pariatur maxime sapiente!",
+		iconTwo: <FaChalkboardTeacher />,
+		headerTextTwo: "Best expert instructor",
+		miniTextTwo:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora possimus voluptatum pariatur maxime sapiente!",
+		image: "./images/student-3.jpg",
+	},
+
+	{
+		id: 2,
+		title: "career",
+		header: "Become A instructor & <br /> join with us",
+		text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.Exercitationem nobis odio facere dolore nesciunt esse evenietvoluptatibus, iure deleniti velit?",
+		iconOne: <GiTeacher />,
+		headerTextOne: "Student friendly",
+		miniTextOne:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora possimus voluptatum pariatur maxime sapiente!",
+		iconTwo: <FaChalkboardTeacher />,
+		headerTextTwo: "Higher qualification",
+		miniTextTwo:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora possimus voluptatum pariatur maxime sapiente!",
+		image: "./images/staff.jpg",
+	},
+];
